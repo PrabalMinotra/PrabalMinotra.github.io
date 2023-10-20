bb1964223db15b8d35d31acd23efc7b57788f560
PRABAL MINOTRA <113827487+PrabalMinotra@users.noreply.github.com>
2023-10-20 09:54:42 +0530

    API fix

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ xhr.addEventListener('readystatechange', function () {
 });
 
 xhr.open('GET', 'https://wordle-answers-solutions.p.rapidapi.com/today');
-xhr.setRequestHeader('X-RapidAPI-Key', 'c22cea6a35msh8686355e38fd89ep15d166jsna9d3f23d956c');
+xhr.setRequestHeader('X-RapidAPI-Key', 'a7db15b896mshaa0eec529d4db5ep1de9ddjsnd0c01ac55756');
 xhr.setRequestHeader('X-RapidAPI-Host', 'wordle-answers-solutions.p.rapidapi.com');
 
 xhr.send();
